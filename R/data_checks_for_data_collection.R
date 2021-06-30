35378874832fc41ff3f4e22cf47ac8b72bb6067a
Anthony Twesigye <twesigye10@gmail.com>
2021-06-30 15:34:16 +0300

    adding other required columns for the log file

--- R/data_checks_for_data_collection.R
+++ R/data_checks_for_data_collection.R
@@ -69,7 +69,16 @@ df_time_verify_new_agents <- df_tool_data %>%
     time_verify_new_agents >= 20
   ) %>% 
   mutate(
-    i.check.identified_issue = "value_outside_limits"
+    i.check.uuid = `_uuid`,
+    i.check.today = today,
+    i.check.enumerator_id = enumerator_id,
+    i.check.identified_issue = "value_outside_limits",
+    i.check.type = NA,
+    i.check.name = "time_verify_new_agents",
+    i.check.value = time_verify_new_agents,
+    i.check.checked_by = "Mathias",
+    i.check.checked_date = as_date(today()),
+    i.check.comment = NA
   )
 
 # 
@@ -79,7 +88,16 @@ df_charge_each_transfer <- df_tool_data %>%
     charge_each_transfer == 999 | charge_each_transfer >= 10000000
   ) %>% 
   mutate(
-    i.check.identified_issue = "value_outside_limits"
+    i.check.uuid = `_uuid`,
+    i.check.today = today,
+    i.check.enumerator_id = enumerator_id,
+    i.check.identified_issue = "value_outside_limits",
+    i.check.type = NA,
+    i.check.name = "charge_each_transfer",
+    i.check.value = charge_each_transfer,
+    i.check.checked_by = "Mathias",
+    i.check.checked_date = as_date(today()),
+    i.check.comment = NA
   )
 
 # •	fixed_fee should be flagged IF response = > 10,000  OR “999”
@@ -87,50 +105,138 @@ df_fixed_fee <- df_tool_data %>%
   filter(
     fixed_fee == 999 | fixed_fee >= 10000
   ) %>% 
-  mutate(i.check.identified_issue = "value_outside_limits")
+  mutate(
+    i.check.uuid = `_uuid`,
+    i.check.today = today,
+    i.check.enumerator_id = enumerator_id,
+    i.check.identified_issue = "value_outside_limits",
+    i.check.type = NA,
+    i.check.name = "fixed_fee",
+    i.check.value = fixed_fee,
+    i.check.checked_by = "Mathias",
+    i.check.checked_date = as_date(today()),
+    i.check.comment = NA
+    )
 # •	withdraw_fixed_fee_amount should be flagged IF response = > 10,000 OR “999”
 df_withdraw_fixed_fee_amount <- df_tool_data %>% 
   filter(
     withdraw_fixed_fee_amount == 999 | withdraw_fixed_fee_amount >= 10000
   ) %>% 
-  mutate(i.check.identified_issue = "value_outside_limits")
+  mutate(
+    i.check.uuid = `_uuid`,
+    i.check.today = today,
+    i.check.enumerator_id = enumerator_id,
+    i.check.identified_issue = "value_outside_limits",
+    i.check.type = NA,
+    i.check.name = "withdraw_fixed_fee_amount",
+    i.check.value = withdraw_fixed_fee_amount,
+    i.check.checked_by = "Mathias",
+    i.check.checked_date = as_date(today()),
+    i.check.comment = NA
+    )
 # •	perc_value_delivered should be flagged IF type_FSP = banking institution AND decimal =  >2 
 # •	perc_value_delivered should be flagged IF response = > 10    OR “999”
 df_perc_value_delivered <- df_tool_data %>% 
   filter(
     (type_FSP == "banking institution" & decimal >= 2) | (perc_value_delivered == 999 | perc_value_delivered >= 10)
   ) %>% 
-  mutate(i.check.identified_issue = "value_outside_limits")
+  mutate(
+    i.check.uuid = `_uuid`,
+    i.check.today = today,
+    i.check.enumerator_id = enumerator_id,
+    i.check.identified_issue = "value_outside_limits",
+    i.check.type = NA,
+    i.check.name = "perc_value_delivered",
+    i.check.value = perc_value_delivered,
+    i.check.checked_by = "Mathias",
+    i.check.checked_date = as_date(today()),
+    i.check.comment = NA
+    )
 
 # •	perc_value_withdraw should be flagged IF response = > 10 OR “999” 
 df_perc_value_withdraw <- df_tool_data %>% 
   filter(
     perc_value_withdraw == 999 | perc_value_withdraw >= 10
   ) %>% 
-  mutate(i.check.identified_issue = "value_outside_limits")
+  mutate(
+    i.check.uuid = `_uuid`,
+    i.check.today = today,
+    i.check.enumerator_id = enumerator_id,
+    i.check.identified_issue = "value_outside_limits",
+    i.check.type = NA,
+    i.check.name = "perc_value_withdraw",
+    i.check.value = perc_value_withdraw,
+    i.check.checked_by = "Mathias",
+    i.check.checked_date = as_date(today()),
+    i.check.comment = NA
+    )
 
 # •	number_agents should be flagged IF response = “999”
 df_number_agents <- df_tool_data %>% 
   filter(
     number_agents == 999
   ) %>% 
-  mutate(i.check.identified_issue = "value_outside_limits")
+  mutate(
+    i.check.uuid = `_uuid`,
+    i.check.today = today,
+    i.check.enumerator_id = enumerator_id,
+    i.check.identified_issue = "value_outside_limits",
+    i.check.type = NA,
+    i.check.name = "number_agents",
+    i.check.value = number_agents,
+    i.check.checked_by = "Mathias",
+    i.check.checked_date = as_date(today()),
+    i.check.comment = NA
+    )
 # •	yes_operate_presence cannot be larger number than number_agents
 df_yes_operate_presence <- df_tool_data %>% 
   filter(
     yes_operate_presence > number_agents
   ) %>% 
-  mutate(i.check.identified_issue = "value_outside_limits")
+  mutate(
+    i.check.uuid = `_uuid`,
+    i.check.today = today,
+    i.check.enumerator_id = enumerator_id,
+    i.check.identified_issue = "value_outside_limits",
+    i.check.type = NA,
+    i.check.name = "yes_operate_presence",
+    i.check.value = yes_operate_presence,
+    i.check.checked_by = "Mathias",
+    i.check.checked_date = as_date(today()),
+    i.check.comment = NA
+    )
 # •	records_kept response should be changed to “all_above” IF “withdrawal” AND “deposit” AND “cash_transfer” are all selected. 
 df_records_kept <- df_tool_data %>% 
   filter(
     grepl("withdrawal", records_kept, ignore.case=TRUE) & grepl("deposit", records_kept, ignore.case=TRUE) & grepl("cash_transfer", records_kept, ignore.case=TRUE) 
   ) %>% 
-  mutate(i.check.identified_issue = "response to be changed to all_above")
+  mutate(
+    i.check.uuid = `_uuid`,
+    i.check.today = today,
+    i.check.enumerator_id = enumerator_id,
+    i.check.identified_issue = "response to be changed to all_above",
+    i.check.type = NA,
+    i.check.name = "records_kept",
+    i.check.value = records_kept,
+    i.check.checked_by = "Mathias",
+    i.check.checked_date = as_date(today()),
+    i.check.comment = NA
+    )
 # •	monitoring_agent_transparency should be flagged IF response “not_applicable”  Does the organization use agents? Then not applicable should not be answered here. 
 df_monitoring_agent_transparency <- df_tool_data %>% 
   filter(
     monitoring_agent_transparency == "not_applicable" & number_agents > 0
       ) %>% 
-  mutate(i.check.identified_issue = "questionable response")
+  mutate(
+    i.check.uuid = `_uuid`,
+    i.check.today = today,
+    i.check.enumerator_id = enumerator_id,
+    i.check.identified_issue = "questionable response",
+    i.check.type = NA,
+    i.check.name = "monitoring_agent_transparency",
+    i.check.value = monitoring_agent_transparency,
+    i.check.checked_by = "Mathias",
+    i.check.checked_date = as_date(today()),
+    i.check.comment = NA
+    )
 
