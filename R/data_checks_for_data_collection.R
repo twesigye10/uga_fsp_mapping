0b4e278562eda4abfb1bfc6de15cd8c7a070eca7
Anthony Twesigye <twesigye10@gmail.com>
2021-07-01 09:21:23 +0300

    modified the file name for the input

--- R/data_checks_for_data_collection.R
+++ R/data_checks_for_data_collection.R
@@ -6,7 +6,7 @@ library(lubridate)
 # rm(list = ls())
 
 # read data ---------------------------------------------------------------
-df_tool_data <- read_csv("inputs/UGA2103_Financial_Service_Providers_Assessment_KI_Tool_June2021_2021-06-24.csv")
+df_tool_data <- read_csv("inputs/UGA2103_Financial_Service_Providers_Assessment_KI_Tool_June2021.csv")
 
 # Time interval for the survey --------------------------------------------
 
