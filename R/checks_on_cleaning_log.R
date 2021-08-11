abe9dc47cd35a9d69dbd2230e2c830f4612b9469
Anthony Twesigye <twesigye10@gmail.com>
2021-08-11 14:10:13 +0300

    outline of steps to take

---
+++ R/checks_on_cleaning_log.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+
+# check if the name exists in the survey questionnaire
+
+# check if all types of change are the expected ones
+
+# check if the type of change and values are consistent with the type of original question
+# select_one, select_multiple
+
+
+
+# check if the value is consistent with other values in that question
+
+
+# create final cleaning log
+
