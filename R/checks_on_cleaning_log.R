d7876684c6aa7bdc9ff4b092d7b87118e62ae4e2
Anthony Twesigye <twesigye10@gmail.com>
2021-08-11 15:22:01 +0300

    detect names in cl not in survey

--- R/checks_on_cleaning_log.R
+++ R/checks_on_cleaning_log.R
@@ -6,10 +6,18 @@ library(janitor)
 df_cleaning_log <- read_excel("inputs/FSP_KII_cleaning_log_final10082021.xlsx", sheet = "log") %>% 
   remove_empty("rows")
 
-df_survey <- read_excel("inputs/UGA2103_FSPA_KI_Tool_June.xlsx", sheet = "survey") 
-df_choices <- read_excel("inputs/UGA2103_FSPA_KI_Tool_June.xlsx", sheet = "choices") 
+df_cl_survey <- read_excel("inputs/UGA2103_FSPA_KI_Tool_June.xlsx", sheet = "survey") 
+df_cl_choices <- read_excel("inputs/UGA2103_FSPA_KI_Tool_June.xlsx", sheet = "choices") 
 
 # check if the name exists in the survey questionnaire
+
+questionnaire_names <- df_cl_survey %>% 
+  filter(str_length(name) > 2) %>% 
+  pull(name) %>% 
+  unique()
+
+cl_name_not_in_svyr <- df_cleaning_log %>% 
+  filter(!name %in% questionnaire_names)
 
 # check if all types of change are the expected ones
 
