c8bf3889bd48d49395d0e29f3374f9e300f45ba5
Anthony Twesigye <twesigye10@gmail.com>
2021-08-12 11:36:09 +0300

    check given type of change for select multiple questions

--- R/checks_on_cleaning_log.R
+++ R/checks_on_cleaning_log.R
@@ -19,14 +19,18 @@ questionnaire_names <- df_cl_survey %>%
 cl_name_not_in_svyr <- df_cleaning_log %>% 
   filter(!name %in% questionnaire_names)
 
-# check if all types of change are the expected ones
+
+# check if all types of change are the expected ones ----------------------
+
 df_cl_check_type <- df_cleaning_log %>%
   left_join(df_cl_survey, by = "name") %>% 
   mutate(qn_type = str_extract(type.y, pattern = "^[a-zA-Z\\_]*"))
 # none select_multiple and not having "change_response" as the type of change
 type_check_none_sm <- df_cl_check_type %>% 
   filter(qn_type != "select_multiple", `type.x` != "change_response") 
-# sm
+# select_multiple and having "change_response" as the type of change
+type_check_sm <- df_cl_check_type %>% 
+  filter(qn_type == "select_multiple", `type.x` == "change_response")
 
 # check if the type of change and values are consistent with the type of original question
 # select_one, select_multiple
