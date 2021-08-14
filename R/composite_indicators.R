899e76a09e0a43e803f824b835ee0b2eb6389117
Anthony Twesigye <twesigye10@gmail.com>
2021-08-14 07:03:51 +0300

    i.number_agents_cat

--- R/composite_indicators.R
+++ R/composite_indicators.R
@@ -7,3 +7,19 @@ library(lubridate)
 
 # read data
 df_tool_data <- read_csv("inputs/UGA2103_FSP_Assessment_Raw_data_aug_final.xlsx")
+
+df_tool_data %>% 
+  mutate(
+    i.number_agents_cat = case_when(
+      number_agents == 0 ~ "0",
+      number_agents >= 1 &  number_agents <= 10 ~ "1 - 10",
+      number_agents >= 11 &  number_agents <= 25 ~ "11 - 25",
+      number_agents >= 25 &  number_agents <= 50 ~ "25 - 50",
+      number_agents >= 51 &  number_agents <= 100 ~ "51 - 100",
+      number_agents >= 101 &  number_agents <= 200 ~ "101 - 200",
+      number_agents >= 201 &  number_agents <= 500 ~ "201 - 500",
+      number_agents >= 501 &  number_agents <= 998 ~ "500+",
+      number_agents == 999 ~ "999",
+      number_agents >= 1000  ~ "500+",
+    )
+  )
