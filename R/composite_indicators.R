5aa6294535632f3e8f320b246dbe3dac4889b187
Anthony Twesigye <twesigye10@gmail.com>
2021-08-14 11:02:50 +0300

    i.operate_presence_cat

--- R/composite_indicators.R
+++ R/composite_indicators.R
@@ -9,40 +9,56 @@ library(lubridate)
 df_tool_data <- read_csv("inputs/UGA2103_FSP_Assessment_Raw_data_aug_final.xlsx")
 
 settlement_regroup <- tibble::tribble(
-                      ~settlement,      ~region,
-                       "adjumani",  "west nile",
-                       "bidibidi",  "west nile",
-                         "imvepi",  "west nile",
-                    "kiryandongo",  "west nile",
-                          "kyaka", "south west",
-                      "kyangwali", "south west",
-                         "lobule",  "west nile",
-                       "nakivale", "south west",
-                      "oruchinga", "south west",
-                        "palabek",  "west nile",
-                      "palorinya",  "west nile",
-                          "rhino",  "west nile",
-                      "rwamwanja", "south west"
-                    )
+  ~settlement,      ~region,
+  "adjumani",  "west nile",
+  "bidibidi",  "west nile",
+  "imvepi",  "west nile",
+  "kiryandongo",  "west nile",
+  "kyaka", "south west",
+  "kyangwali", "south west",
+  "lobule",  "west nile",
+  "nakivale", "south west",
+  "oruchinga", "south west",
+  "palabek",  "west nile",
+  "palorinya",  "west nile",
+  "rhino",  "west nile",
+  "rwamwanja", "south west"
+)
 
 region_lookup <- setNames(object = settlement_regroup$region, nm = settlement_regroup$settlement)
 
 
 df_tool_data <- df_tool_data %>% 
   mutate(
-    i.number_agents_cat = case_when(
-      number_agents == 0 ~ "0",
-      number_agents >= 1 &  number_agents <= 10 ~ "1 - 10",
-      number_agents >= 11 &  number_agents <= 25 ~ "11 - 25",
-      number_agents >= 25 &  number_agents <= 50 ~ "25 - 50",
-      number_agents >= 51 &  number_agents <= 100 ~ "51 - 100",
-      number_agents >= 101 &  number_agents <= 200 ~ "101 - 200",
-      number_agents >= 201 &  number_agents <= 500 ~ "201 - 500",
-      number_agents >= 501 &  number_agents <= 998 ~ "500+",
-      number_agents == 999 ~ "999",
-      number_agents >= 1000  ~ "500+",
-      TRUE ~ as.character(number_agents)
+    i.number_agents_cat = case_when(number_agents == 0 ~ "0",
+                                    number_agents >= 1 &  number_agents <= 10 ~ "1 - 10",
+                                    number_agents >= 11 &  number_agents <= 25 ~ "11 - 25",
+                                    number_agents >= 25 &  number_agents <= 50 ~ "25 - 50",
+                                    number_agents >= 51 &  number_agents <= 100 ~ "51 - 100",
+                                    number_agents >= 101 &  number_agents <= 200 ~ "101 - 200",
+                                    number_agents >= 201 &  number_agents <= 500 ~ "201 - 500",
+                                    number_agents >= 501 &  number_agents <= 998 ~ "500+",
+                                    number_agents == 999 ~ "999",
+                                    number_agents >= 1000  ~ "500+",
+                                    TRUE ~ "999"
     ),
-    i.region = recode(refugee_settlelement, !!!region_lookup)
+    i.region = recode(refugee_settlelement, !!!region_lookup),
+    i.operate_presence_cat = case_when(yes_operate_presence == 0 ~ "0",
+                                       yes_operate_presence >= 1 & yes_operate_presence <= 10 ~   "1 - 10",
+                                       yes_operate_presence >= 11 & yes_operate_presence <= 20 ~  "11 - 20",
+                                       yes_operate_presence >= 21 & yes_operate_presence <= 30 ~  "21 - 30",
+                                       yes_operate_presence >= 31 & yes_operate_presence <= 40 ~  "31 - 40",
+                                       yes_operate_presence >= 41 & yes_operate_presence <= 50 ~  "41 - 50",
+                                       yes_operate_presence >= 51 & yes_operate_presence <= 75 ~  "51 - 75",
+                                       yes_operate_presence >= 76 & yes_operate_presence <= 100 ~ "76 - 100",
+                                       yes_operate_presence >= 101 & yes_operate_presence <= 998 ~    "100 +",
+                                       yes_operate_presence == 999 ~      "999",
+                                       yes_operate_presence >= 1000 ~      "100 +",
+                                       TRUE ~ "999"
+    )
+    
   )
+
+df_tool_data %>% 
+  tabyl(i.region)
 
