cf0094cdd1211b83d37b26f91f55663caac88589
Anthony Twesigye <twesigye10@gmail.com>
2021-08-14 11:26:47 +0300

    reading excel

--- R/composite_indicators.R
+++ R/composite_indicators.R
@@ -6,7 +6,7 @@ library(janitor)
 library(lubridate)
 
 # read data
-df_tool_data <- read_csv("inputs/UGA2103_FSP_Assessment_Raw_data_aug_final.xlsx")
+df_tool_data <- read_excel("inputs/UGA2103_FSP_Assessment_Raw_data_aug_final.xlsx")
 
 settlement_regroup <- tibble::tribble(
   ~settlement,      ~region,
