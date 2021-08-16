8e9997b146fead95c0acae2820ce0f1cf857f0fe
Anthony Twesigye <twesigye10@gmail.com>
2021-08-16 18:55:36 +0300

    adding other fields to the cleaning log

--- R/implement_cleaning_log.R
+++ R/implement_cleaning_log.R
@@ -4,7 +4,8 @@ library(tidyverse)
 library(lubridate)
 
 # read the data
-df_cleaning_log <- readxl::read_excel("inputs/FSP_KII_cleaning_log_final13082021.xlsx")
+df_cleaning_log <- readxl::read_excel("inputs/FSP_KII_cleaning_log_final13082021.xlsx") %>% 
+  mutate(sheet = NA, index = NA, relevant = NA)
 df_raw_data <- readxl::read_excel("inputs/UGA2103_FSP_Assessment_Raw_data_aug_final.xlsx")
 df_survey <- readxl::read_excel("inputs/UGA2103_FSP_Tool_June2021_Final_2021_08_12.xlsx", sheet = "survey") 
 df_choices <- readxl::read_excel("inputs/UGA2103_FSP_Tool_June2021_Final_2021_08_12.xlsx", sheet = "choices") 
