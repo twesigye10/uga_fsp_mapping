930f46ed1ddaa5bc5faef7004b10a144496667eb
Anthony Twesigye <twesigye10@gmail.com>
2021-08-16 07:14:35 +0300

    steps for the implementing the cleaning log

---
+++ R/implement_cleaning_log.R
@@ -0,0 +1,18 @@
+# implement cleaning log
+
+
+# find all new choices to add to choices sheet ----------------------------
+
+
+# add new choices to the survey tool --------------------------------------
+
+
+# modify the data using the cleaning log ----------------------------------
+
+
+# make some cleanup -------------------------------------------------------
+
+
+# write modified data -----------------------------------------------------
+
+
