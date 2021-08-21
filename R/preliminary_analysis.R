c6575534f52d0d08c4938f6e8a37491e0d909aaf
Anthony Twesigye <twesigye10@gmail.com>
2021-08-21 06:09:03 +0300

    load data

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -3,3 +3,11 @@ library(janitor)
 library(glue)
 
 source("R/composite_indicators.R")
+
+
+# load data ---------------------------------------------------------------
+
+df_cleaned <- read_csv("outputs/20210820_clean_data.csv")
+
+dap <- read_csv("inputs/r_dap.csv") %>% 
+  janitor::clean_names()
