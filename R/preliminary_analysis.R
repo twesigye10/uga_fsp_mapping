65b4aac7daf2d511052e3183b4a62a617efdbd92
Anthony Twesigye <twesigye10@gmail.com>
2021-08-21 06:11:10 +0300

    composite indicators

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -11,3 +11,8 @@ df_cleaned <- read_csv("outputs/20210820_clean_data.csv")
 
 dap <- read_csv("inputs/r_dap.csv") %>% 
   janitor::clean_names()
+
+
+# make composite indicator ------------------------------------------------
+
+df_with_composites <- create_composite_indicators_fsp(input_df = df_cleaned)
